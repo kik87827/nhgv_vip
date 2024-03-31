d6bb6b5089e2d845d48b278322187a021fb60466
kik8727 <kik87827@gmail.com>
2024-03-31 21:33:25 +0900

    페이지 세팅 완료

---
+++ dist/assets/js/ui.js
@@ -0,0 +1,63 @@
+window.addEventListener("DOMContentLoaded", () => {
+  commonInit();
+});
+window.addEventListener("load", () => {
+  //layoutFunc();
+});
+
+$(function() {
+})
+
+/**
+ * device check
+ */
+function commonInit() {
+  let touchstart = "ontouchstart" in window;
+  let userAgent = navigator.userAgent.toLowerCase();
+  if (touchstart) {
+    browserAdd("touchmode");
+  }
+  if (userAgent.indexOf("samsung") > -1) {
+    browserAdd("samsung");
+  }
+
+  if (
+    navigator.platform.indexOf("Win") > -1 ||
+    navigator.platform.indexOf("win") > -1
+  ) {
+    browserAdd("window");
+  }
+
+  if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
+    // iPad or iPhone
+    browserAdd("ios");
+  }
+
+  function browserAdd(opt) {
+    document.querySelector("html").classList.add(opt);
+  }
+}
+
+/*
+  resize
+*/
+function resizeAction(callback) {
+  let windowWid = 0;
+  window.addEventListener("resize", () => {
+    if (window.innerWidth !== windowWid) {
+      if (callback) {
+        callback();
+      }
+    }
+    windowWid = window.innerWidth;
+  });
+}
+
+
+function menuRock(item){
+  $(function(){
+    if(!!item){
+      $(item).addClass("active");
+    }
+  });
+}
